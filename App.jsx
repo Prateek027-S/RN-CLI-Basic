799fd3c2988a54b3a51d5c55d09f09dbe2074a07
Prateek Shah <shahprateek02@gmail.com>
2026-04-28 14:07:26 +0530

    feat: Theming via Context API

--- App.jsx
+++ App.jsx
@@ -6,29 +6,57 @@
  */
 import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar, useColorScheme } from 'react-native';
+import { StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import {
   SafeAreaProvider
 } from 'react-native-safe-area-context';
 import DetailsScreen from './src/screens/details/DetailsScreen';
 import HomeScreen from './src/screens/home/HomeScreen';
+import { useState } from 'react';
+import { MyThemeContext } from './ThemeContext';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [theme, setTheme] = useState("light");
+  const isDark = (theme === "dark");
+
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === "light" ? "dark" : "light"));
+  }
 
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Details" component={DetailsScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <MyThemeContext.Provider value={{theme, toggleTheme}}>
+        <StatusBar barStyle={isDark ? 'light-content' : 'dark-content'} />
+        <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+        <TouchableOpacity
+          onPress={toggleTheme} 
+          style={styles.toggleButton(isDark)}>
+          <Text style={{color: isDark ? "white" : "black"}}>Toggle Theme</Text>
+        </TouchableOpacity>
+      </MyThemeContext.Provider>
     </SafeAreaProvider>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  toggleButton: (isDark) => ({
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    padding: 6, 
+    position: 'absolute', 
+    bottom: 130, 
+    right: 20, 
+    borderWidth: 1, 
+    borderRadius: 60,
+    backgroundColor: isDark ? "gray" : "white" 
+  })
+})
--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,12 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
+import { useContext } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import {MyThemeContext} from "../../../ThemeContext"
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const {theme} = useContext(MyThemeContext);
+  const isDark = theme === "dark";
 
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
+    <View style={[
+      styles.container, 
+      { backgroundColor: isDark ? '#44444E' : 'white' }
+    ]}>
+      <Text style={{color: isDark ? "white" : "black"}}>HomeScreen</Text>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Details')}>
         <Text style={{color: 'white'}}>Go to Details</Text>
       </TouchableOpacity>
